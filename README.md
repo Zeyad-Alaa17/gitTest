09a65e5e3dd6de9b778dc11fc8412f063fdcb84f
Zeyad-Alaa17 <56205913+Zeyad-Alaa17@users.noreply.github.com>
2023-02-06 17:27:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Weather-Journal App Project
+
+
+
+# Demo-Preview
+
+<!-- Add a demo for your project -->
+
+<!-- After you have written about your project, it is a good idea to have a demo/preview(**video/gif/screenshots** are good options) of your project so that people can know what to expect in your project. You could also add the demo in the previous section with the product description.
+Here is a random GIF as a placeholder.
+ -->
+
+![Imgur Image](https://i.imgur.com/4oiY1qU.png)
+
+# Project description
+This is an asynchronous web app that uses Web API and user data to dynamically update the UI with current date and weather for the specified US zipcode.
+
+# Usage
+open command line in the following directory projects/weather-journal-app/
+Run the server using the following command: "node ./server.js"
+
+<!-- This is optional and it is used to give the user info on how to use the project after installation. This could be added in the Installation section also. -->
+
+# Credits
+Zeyad Alaa Eldin Mohamed
---
+++ website/app.js
@@ -0,0 +1,76 @@
+/* Global Variables */
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = (d.getMonth()+1) + '-' + d.getDate() + '-' + d.getFullYear();
+
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?'
+const apiKey = 'appid=e0b77c3ea2f0d5cfa389696b6e84d07d&units=imperial';
+
+// Async GET
+getData = async (url: string) => {
+    const request = await fetch(url);
+    try {
+        // Transform into JSON
+        const res = await request.json()
+        return (res)
+    } catch (error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
+};
+products = this.getData("admin/products").then((r) => { console.log(this.products = r) })
+const getData = async (url) => {
+    const request = await fetch(url);
+    try {
+        // Transform into JSON
+        const data = await request.json()
+        if (data.cod !== 200) alert("please enter a valid US zipcode")
+        else return (data)
+    } catch (error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
+};
+const postData = async (url = '', data = {}) => {
+
+    const response = await fetch(url, {
+        method: 'POST', credentials: 'same-origin', headers: {
+            'Content-Type': 'application/json',
+        }, body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+
+    try {
+        return await response.json();
+    } catch (error) {
+        console.log("error", error);
+    }
+};
+
+
+async function getWeather() {
+    let zipCode = "&zip=" + document.getElementById('zip').value;
+    let content = document.getElementById("feelings").value;
+
+    getData(baseURL + apiKey + zipCode).then(newData => {
+        postData("/addData", {date: newDate, temp: newData.main.temp, content});
+    }).then(updateUI)
+}
+
+document.getElementById("generate").addEventListener("click", getWeather)
+
+
+async function updateUI() {
+    const request = await fetch('/all');
+    try {
+        const allData = await request.json();
+        document.getElementById("entry").classList.remove("invisible");
+        document.getElementById('date').innerText = allData.date;
+        document.getElementById('temp').innerHTML = Math.round(allData.temp) + '°F';
+        document.getElementById('content').innerHTML = allData.content;
+
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+
